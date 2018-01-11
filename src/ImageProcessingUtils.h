6cedd745ce8cc7bbccff2624370d103336571dcc
Peter Dedecker <peter.dedecker@hotmail.com>
2018-01-11 17:01:45 +0100

    Use cropping for the image rotation to enhance performance.

--- src/ImageProcessingUtils.h
+++ src/ImageProcessingUtils.h
@@ -1,30 +1,44 @@
 #ifndef IMAGEPROCESSINGUTILS_H
 #define IMAGEPROCESSINGUTILS_H
 
+const size_t kBlockSize = 32;
+
 template <typename T>
 void RotateCW(T* image, size_t nRows, size_t nCols, T* rotatedImage) {
-    size_t offset = 0;
-    for (size_t col = 0; col < nCols; ++col) {
-        for (size_t row = 0; row < nRows; ++row) {
-            size_t newRow = nCols - 1 - col;
-            size_t newCol = row;
-            size_t idx = RowColToLinear(nCols, newRow, newCol);
-            rotatedImage[idx] = image[offset];
-            offset += 1;
+    size_t blockSize = std::min(std::min(nRows, nCols), kBlockSize);
+    #pragma omp parallel for num_threads(2)
+    for (int rowOffset = 0; rowOffset < nRows; rowOffset += blockSize) {
+        for (size_t colOffset = 0; colOffset < nCols; colOffset += blockSize) {
+            for (size_t col = colOffset; col < colOffset + blockSize; ++col) {
+                size_t inputIdx = col * nRows + rowOffset;
+                for (size_t row = rowOffset; row < rowOffset + blockSize; ++row) {
+                    size_t newRow = col;
+                    size_t newCol = nRows - 1 - row;
+                    size_t idx = RowColToLinear(nCols, newRow, newCol);
+                    rotatedImage[idx] = image[inputIdx];
+                    inputIdx += 1;
+                }
+            }
         }
     }
 }
 
 template <typename T>
 void RotateCCW(T* image, size_t nRows, size_t nCols, T* rotatedImage) {
-    size_t offset = 0;
-    for (size_t col = 0; col < nCols; ++col) {
-        for (size_t row = 0; row < nRows; ++row) {
-            size_t newRow = col;
-            size_t newCol = nRows - 1 - row;
-            size_t idx = RowColToLinear(nCols, newRow, newCol);
-            rotatedImage[idx] = image[offset];
-            offset += 1;
+    size_t blockSize = std::min(std::min(nRows, nCols), kBlockSize);
+    #pragma omp parallel for num_threads(2)
+    for (int rowOffset = 0; rowOffset < nRows; rowOffset += blockSize) {
+        for (size_t colOffset = 0; colOffset < nCols; colOffset += blockSize) {
+            for (size_t col = colOffset; col < colOffset + blockSize; ++col) {
+                size_t inputIdx = col * nRows + rowOffset;
+                for (size_t row = rowOffset; row < rowOffset + blockSize; ++row) {
+                    size_t newRow = nCols - 1 - col;
+                    size_t newCol = row;
+                    size_t idx = RowColToLinear(nCols, newRow, newCol);
+                    rotatedImage[idx] = image[inputIdx];
+                    inputIdx += 1;
+                }
+            }
         }
     }
 }
--- src/SCConfigure.h
+++ src/SCConfigure.h
@@ -3,8 +3,8 @@
 
 //#define WITH_PHOTOMETRICS
 //#define WITH_ANDOR
-//#define WITH_DUMMYCAM
+#define WITH_DUMMYCAM
 //#define WITH_HAMAMATSU
-#define WITH_IDS
+//#define WITH_IDS
 
 #endif
