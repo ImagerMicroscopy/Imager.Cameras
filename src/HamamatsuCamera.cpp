5ea5cbd8031585fad322c0b4517b5306c9a271e4
Peter Dedecker <peter.dedecker@hotmail.com>
2018-10-03 15:13:48 +0200

    Shave 30 ms off single camera acquisition by only allocating the image buffer once for Hamamatsu.

--- src/HamamatsuCamera.cpp
+++ src/HamamatsuCamera.cpp
@@ -233,23 +233,25 @@ void HamamatsuCamera::_derivedSetBinningFactor(const int binningFactor) {
 void HamamatsuCamera::_derivedStartAsyncAcquisition() {
 	std::pair<int, int> imageSize = getActualImageSize();
 	int nPixelsInImage = imageSize.first * imageSize.second;
+	DCAMERR err;
 	
-	_frameBuffer.resize(nPixelsInImage * kHamamatsuImagesInBuffer);
+	if (_frameBuffer.empty()) {
+		_frameBuffer.resize(nPixelsInImage * kHamamatsuImagesInBuffer);
 
-    DCAMERR err;
-    std::uint16_t* bufferPtrs[kHamamatsuImagesInBuffer];
-    for (int i = 0; i < kHamamatsuImagesInBuffer; i++) {
-        bufferPtrs[i] = _frameBuffer.data() + i * nPixelsInImage;
-    }
-    DCAMBUF_ATTACH attachParams = { 0 };
-    attachParams.size = sizeof(attachParams);
-    attachParams.iKind = DCAMBUF_ATTACHKIND_FRAME;
-    attachParams.buffer = reinterpret_cast<void**>(&bufferPtrs);
-    attachParams.buffercount = kHamamatsuImagesInBuffer;
-    err = dcambuf_attach(_camHandle, &attachParams);
-    if (err != DCAMERR_SUCCESS) {
-        throw std::runtime_error("couldn't attach buffers");
-    }
+		std::uint16_t* bufferPtrs[kHamamatsuImagesInBuffer];
+		for (int i = 0; i < kHamamatsuImagesInBuffer; i++) {
+			bufferPtrs[i] = _frameBuffer.data() + i * nPixelsInImage;
+		}
+		DCAMBUF_ATTACH attachParams = { 0 };
+		attachParams.size = sizeof(attachParams);
+		attachParams.iKind = DCAMBUF_ATTACHKIND_FRAME;
+		attachParams.buffer = reinterpret_cast<void**>(&bufferPtrs);
+		attachParams.buffercount = kHamamatsuImagesInBuffer;
+		err = dcambuf_attach(_camHandle, &attachParams);
+		if (err != DCAMERR_SUCCESS) {
+			throw std::runtime_error("couldn't attach buffers");
+		}
+	}
 
 	if (_camWaitHandle == nullptr) {
 		DCAMWAIT_OPEN waitParams = { 0 };
@@ -278,9 +280,6 @@ void HamamatsuCamera::_derivedAbortAsyncAcquisition() {
 		dcamwait_close(_camWaitHandle);
 		_camWaitHandle = nullptr;
 	}
-
-    dcambuf_release(_camHandle);
-    _frameBuffer.clear();
 }
 
 bool HamamatsuCamera::_waitForNewImageWithTimeout(int timeoutMillis) {
