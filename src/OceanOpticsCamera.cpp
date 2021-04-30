0e12f671513b874f892c8848fe8d6635c9efb899
Peter Dedecker <peter.dedecker@hotmail.com>
2021-04-30 09:03:39 +0200

    More work on Ocean Optics.

--- src/OceanOpticsCamera.cpp
+++ src/OceanOpticsCamera.cpp
@@ -8,7 +8,10 @@
 
 OceanOpticsCamera::OceanOpticsCamera(long deviceID) :
 	_seabreezeAPI(SeaBreezeAPI::getInstance()),
-	_deviceID(deviceID)
+	_deviceID(deviceID),
+	_nSpectraToAverage(1),
+	_spectraGrabberShouldAbort(false),
+	_spectraGrabberHasError(false)
 {
 	int errorCode = 0, err = 0;
 
@@ -51,6 +54,46 @@ std::string OceanOpticsCamera::getIdentifierStr() const {
 	return std::string(buf);
 }
 
+std::vector<CameraProperty> OceanOpticsCamera::_derivedGetCameraProperties() {
+	return { _getSetExposureTime(GetProperty, 0.0), _getSetNSpectraToAverage(GetProperty, 0.0) };
+}
+
+void OceanOpticsCamera::_derivedSetCameraProperties(const std::vector<CameraProperty>& properties) {
+	for (const auto& prop : properties) {
+		switch (prop.getPropertyCode()) {
+		case PropExposureTime:
+			_getSetExposureTime(SetProperty, prop.getValue());
+			break;
+		case PropNSpectraToAverage:
+			_getSetNSpectraToAverage(SetProperty, prop.getValue());
+			break;
+		default:
+			throw std::runtime_error("setting unrecognized option");
+		}
+	}
+}
+
+std::pair<int, int> OceanOpticsCamera::_getSizeOfRawImages() const {
+	return std::pair<int, int>(_spectrumLength, 1);
+}
+
+CameraProperty OceanOpticsCamera::_getSetExposureTime(GetOrSetProperty getOrSet, const double exposureTime) {
+	if (getOrSet == SetProperty) {
+		_setExposureTime(exposureTime);
+	}
+	CameraProperty prop(PropExposureTime, "Exposure time");
+	prop.setNumeric(_getExposureTime());
+	return prop;
+}
+
+CameraProperty OceanOpticsCamera::_getSetNSpectraToAverage(GetOrSetProperty getOrSet, const double nSpectra) {
+	if (getOrSet == SetProperty) {
+		_setNSpectraToAverage(nSpectra);
+	}
+	CameraProperty prop(PropNSpectraToAverage, "N spectra to average");
+	prop.setNumeric(_getNSpectraToAverage());
+}
+
 void OceanOpticsCamera::_setDefaults() {
 	_setExposureTime(0.1);
 }
@@ -59,7 +102,8 @@ void OceanOpticsCamera::_setExposureTime(const double exposureTime) {
 	int errorCode = 0;
 	long integrationTimeMicros = static_cast<long>(exposureTime * 1.0e6);
 	integrationTimeMicros = clamp(integrationTimeMicros, _minIntegrationTimeMicros, _maxIntegrationTimeMicros);
-	_seabreezeAPI->spectrometerSetIntegrationTimeMicros(_deviceID, 0, &errorCode);
+	integrationTimeMicros = std::min(integrationTimeMicros, 1000000L);	// limit to max 1 s
+	_seabreezeAPI->spectrometerSetIntegrationTimeMicros(_deviceID, 0, &errorCode, integrationTimeMicros);
 	if (errorCode != 0) {
 		throw std::runtime_error("error setting exposure time");
 	}
@@ -68,6 +112,107 @@ void OceanOpticsCamera::_setExposureTime(const double exposureTime) {
 
 double OceanOpticsCamera::_getExposureTime() const {
 	return _integrationTimeMicros;
+}
+
+void OceanOpticsCamera::_setNSpectraToAverage(const double nSpectra) {
+	_nSpectraToAverage = clamp(nSpectra, 1.0, 1000.0);
+}
+
+double OceanOpticsCamera::_getNSpectraToAverage() const {
+	return _nSpectraToAverage;
+}
+
+void OceanOpticsCamera::_derivedStartAsyncAcquisition() {
+	_startAsyncSpectraGrabber();
+}
+
+void OceanOpticsCamera::_derivedAbortAsyncAcquisition() {
+	_stopAsyncSpectraGrabber();
+}
+
+bool OceanOpticsCamera::_derivedNewAsyncAcquisitionImageAvailable() {
+	return (_availableSpectraQueue.size_approx() > 0);
+}
+
+bool OceanOpticsCamera::_waitForNewImageWithTimeout(int timeoutMillis) {
+	if (_spectraGrabberHasError) {
+		_stopAsyncSpectraGrabber();
+		throw std::runtime_error("error in acquisition thread");
+	}
+	bool haveIt = _availableSpectraQueue.wait_dequeue_timed(_spectrumInFlight, std::chrono::milliseconds(timeoutMillis));
+	return haveIt;
+}
+
+void OceanOpticsCamera::_derivedStoreNewImageInBuffer(std::uint16_t * bufferForThisImage, int nBytes) {
+	if (nBytes != (_spectrumInFlight.size() * sizeof(std::uint16_t))) {
+		throw std::runtime_error("buffer of wrong size");
+	}
+
+	for (size_t i = 0; i < _spectrumInFlight.size(); i += 1) {
+		bufferForThisImage[i] = std::round(_spectrumInFlight[i]);
+	}
+}
+
+void OceanOpticsCamera::_asyncSpectraGrabberWorker() {
+	int nPixels = _getSizeOfRawImages().first;
+	int nSpectraToAverage = _nSpectraToAverage;
+	std::vector<double> accumulatedSpectrum(nPixels);
+	std::vector<double> singleSpectrum(nPixels);
+
+	// discard the first spectrum
+	int errorCode = 0, err = 0;
+	err = _seabreezeAPI->spectrometerGetFormattedSpectrum(_deviceID, _featureID, &errorCode, singleSpectrum.data(), singleSpectrum.size());
+	if (err) {
+		_spectraGrabberHasError = true;
+		return;
+	}
+
+
+	for ( ; ; ) {
+		for (auto& p : accumulatedSpectrum) {
+			p = 0.0;
+		}
+
+		for (int i = 0; i < nSpectraToAverage; i += 1) {
+			if (_spectraGrabberShouldAbort) {
+				return;
+			}
+
+			err = _seabreezeAPI->spectrometerGetFormattedSpectrum(_deviceID, _featureID, &errorCode, singleSpectrum.data(), singleSpectrum.size());
+			if (err) {
+				_spectraGrabberHasError = true;
+				return;
+			}
+
+			for (size_t i = 0; i < accumulatedSpectrum.size(); i += 1) {
+				accumulatedSpectrum[i] += singleSpectrum[i];
+			}
+		}
+		if (nSpectraToAverage > 1) {
+			for (auto& p : accumulatedSpectrum) {
+				p /= (double)nSpectraToAverage;
+			}
+		}
+
+		_availableSpectraQueue.enqueue(accumulatedSpectrum);
+	}
+}
+
+void OceanOpticsCamera::_startAsyncSpectraGrabber() {
+	_stopAsyncSpectraGrabber();
+	_spectraGrabberShouldAbort = false;
+	_spectraGrabberHasError = false;
+	_spectraGrabberFuture = std::async(std::launch::async, [=]() {
+		_asyncSpectraGrabberWorker();
+	});
+}
+
+void OceanOpticsCamera::_stopAsyncSpectraGrabber() {
+	_spectraGrabberShouldAbort = true;
+	if (_spectraGrabberFuture.valid()) {
+		_spectraGrabberFuture.wait();
+		_spectraGrabberFuture.get();
+	}
 }
 
 #endif
--- src/OceanOpticsCamera.h
+++ src/OceanOpticsCamera.h
@@ -3,23 +3,57 @@
 
 #include "BaseCameraClass.h"
 
+#include <future>
 #include <string>
 #include <vector>
+
+#include "ReaderWriterQueue/atomicops.h"
+#include "ReaderWriterQueue/readerwriterqueue.h"
 
 #include "api/seabreezeapi/SeaBreezeAPI.h"
 
 class OceanOpticsCamera : public BaseCameraClass {
 public:
+	enum GetOrSetProperty {
+		GetProperty,
+		SetProperty
+	};
+
+	enum OOPropIDs {
+		PropExposureTime = CameraProperty::FirstAvailablePropertyID,
+		PropNSpectraToAverage
+	};
+
 	OceanOpticsCamera(long deviceID);
 	virtual ~OceanOpticsCamera();
 
 	std::string getIdentifierStr() const final;
 
 private:
+	std::vector<CameraProperty> _derivedGetCameraProperties() override;
+	void _derivedSetCameraProperties(const std::vector<CameraProperty>& properties) override;
+
+	std::pair<int, int> _getSizeOfRawImages() const override;
+
+	CameraProperty _getSetExposureTime(GetOrSetProperty getOrSet, const double exposureTime);
+	CameraProperty _getSetNSpectraToAverage(GetOrSetProperty getOrSet, const double nSpectra);
+
 	void _setDefaults();
 
 	void _setExposureTime(const double exposureTime);
 	double _getExposureTime() const;
+	void _setNSpectraToAverage(const double nSpectra);
+	double _getNSpectraToAverage() const;
+
+	void _derivedStartAsyncAcquisition() override;
+	void _derivedAbortAsyncAcquisition() override;
+	bool _derivedNewAsyncAcquisitionImageAvailable() override;
+	bool _waitForNewImageWithTimeout(int timeoutMillis) override;
+	void _derivedStoreNewImageInBuffer(std::uint16_t* bufferForThisImage, int nBytes) override;
+
+	void _asyncSpectraGrabberWorker();
+	void _startAsyncSpectraGrabber();
+	void _stopAsyncSpectraGrabber();
 
 	SeaBreezeAPI* _seabreezeAPI;
 	long _deviceID;
@@ -29,6 +63,13 @@ private:
 	long _integrationTimeMicros;
 	long _minIntegrationTimeMicros;
 	long _maxIntegrationTimeMicros;
+	int _nSpectraToAverage;
+
+	std::future<void> _spectraGrabberFuture;
+	bool _spectraGrabberShouldAbort;
+	bool _spectraGrabberHasError;
+	moodycamel::BlockingReaderWriterQueue<std::vector<double>> _availableSpectraQueue;
+	std::vector<double> _spectrumInFlight;
 };
 
 #endif
